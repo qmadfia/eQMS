c5dae1d8ef4d8f186a090fa5006352a4fd55dda7
qmadfia <qm.adfia@gmail.com>
2024-12-11 09:40:17 +0700

    add CSS dan Js

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript File
+
+// Example function to handle save button click
+document.querySelector('.save-button').addEventListener('click', () => {
+    alert('Save button clicked!');
+});
+
+// Example function to handle FTT button click
+document.querySelector('.ftt-button').addEventListener('click', () => {
+    alert('FTT button clicked!');
+});
+
+// Add more JavaScript functionality here if needed
